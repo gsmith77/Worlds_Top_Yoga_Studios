daff0a45735aa7656d530c5ec16b7b6a0774bd9d
grant smith <grantsmith1256@gmail.com>
2018-12-15 18:09:15 +0000

    Scraping Index

--- lib/Worlds_Top_Yoga_Studios/scraper.rb
+++ lib/Worlds_Top_Yoga_Studios/scraper.rb
@@ -1,11 +1,11 @@
 class WorldsTopYogaStudios::Scraper
   
   def get_page
-    doc = Nokogiri::HTML(open(https://www.yogiapproved.com/travel/9-uniquely-beautiful-yoga-studios-around-the-world/))
+    doc = Nokogiri::HTML(open("https://www.yogiapproved.com/travel/9-uniquely-beautiful-yoga-studios-around-the-world/"))
   end
   
   def scrape_studios_index
-    doc
+    self.get_page.css("div.body-text__content")
   end
   
 end
