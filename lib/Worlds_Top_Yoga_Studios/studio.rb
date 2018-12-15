4f7f9c5c8e403bee5747c1493570f916aae08f86
grant smith <grantsmith1256@gmail.com>
2018-12-15 19:04:07 +0000

    Added Studio.rb

---
+++ lib/Worlds_Top_Yoga_Studios/studio.rb
@@ -0,0 +1,12 @@
+class WorldsTopYogaStudios::Studio
+  
+  def self.studios
+    
+    studio_1 = Studio.new
+    studio_1.name = "Amrita Yoga and Wellness"
+    studio_1.location = "Philadelphia, Pennsylvania"
+    studio_1.url = "mritayogawellness.com"
+  end
+  
+  
+end
