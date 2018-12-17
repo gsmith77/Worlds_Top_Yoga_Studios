a23b3b440ca39f1f9be75619c15bebf3a87813aa
grant smith <grantsmith1256@gmail.com>
2018-12-17 04:32:13 +0000

    studio.rb enchancing

--- lib/Worlds_Top_Yoga_Studios/studio.rb
+++ lib/Worlds_Top_Yoga_Studios/studio.rb
@@ -2,19 +2,33 @@ class WorldsTopYogaStudios::Studio
   
   attr_accessor :name, :location, :url
   
+  def self.new_from_index_page(s)
+    
+  end
+  
+  def intialize(name=nil, location=nil, url=nil, bio=nil)
+    @name = name
+    @url = url
+    @location = location
+    @bio = bio
+    @@all << self
+  end
+  end
+  
   def self.studios
+    list_of_studios = []
     
-    studio_1 = self.new
-    studio_1.name = "Amrita Yoga and Wellness"
-    studio_1.location = "Philadelphia, Pennsylvania"
-    studio_1.url = "mritayogawellness.com"
+    #studio_1 = self.new
+    #studio_1.name = "Amrita Yoga and Wellness"
+    #studio_1.location = "Philadelphia, Pennsylvania"
+    #studio_1.url = "mritayogawellness.com"
     
-    studio_2 = self.new
-    studio_2.name = "GRRRRRRant"
-    studio_2.location = "YOOO"
-    studio_2.url = "WHY>"
+    #studio_2 = self.new
+    #studio_2.name = "GRRRRRRant"
+    #studio_2.location = "YOOO"
+    #studio_2.url = "WHY>"
     
-    [studio_1, studio_2]
+    list_of_studios
   end
   
   
