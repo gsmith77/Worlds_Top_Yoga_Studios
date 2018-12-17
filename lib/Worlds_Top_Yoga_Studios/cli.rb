0ec7823fc88bd77e34dc1e37afc9b30256f3c425
grant smith <grantsmith1256@gmail.com>
2018-12-17 06:10:43 +0000

    cli

--- lib/Worlds_Top_Yoga_Studios/cli.rb
+++ lib/Worlds_Top_Yoga_Studios/cli.rb
@@ -1,8 +1,9 @@
 class WorldsTopYogaStudios::CLI
   
   def call
+    WorldsTopYogaStudios::Scraper.new.make_studios
     puts "Here are 9 amazing yoga studios"
-    list_studios
+    start
   end
   
   def list_studios
@@ -13,24 +14,34 @@ class WorldsTopYogaStudios::CLI
     end
   end
   
-  def menu
-    input = ""
-    while input != "exit"
-    puts "            "
+  def start
+    puts ""
     puts "Enter the number of which yoga studio you would like more information about or hit list to display the studios again:"
-    input = gets.strip.downcase
     
-    if input.to_i > 0
-      @all_studios[input.to_i - 1]
-      puts "#{@all_studios.name} - #{@all_studios.location} - #{@all_studios.url}"
-      elsif input == "list"
-      list_studios
-    else 
-      puts "            "
-      puts "Not sure what you meant... Please enter 1-9 or exit"
-    end
+    input = gets.strip
+    
+    studio = WorldsTopYogaStudios::Studio.find(input.to_i)
+    
+    print_studio(studio)
+    
+    puts ""
+    puts "Would you like to see another studio? Y or N"
+    if "Y"
+      start
+      elsif "N"
+      goodbye
+    else ""
+      puts "Sorry did not understand that please tyoe 1-9 to get more information on a studio."
+    
+  def print_studio(studio)
+    puts ""
+    puts "---------- #{studio.name} -----------"
+    puts ""
+    puts "Location:  #{studio.location}"
+    puts "Website:   #{studio.url}"
+    puts ""
   end
-  
+    
   def goodbye
     puts "See you later! As always, namaste."
   end
